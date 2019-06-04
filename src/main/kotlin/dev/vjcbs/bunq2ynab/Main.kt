f65c3c67ef3200ef47f5dc7b353c8add11537142
Victor Jacobs <victor@vjcbs.dev>
2019-06-04 17:53:10 +0200

    Don't fall over when calling YNAB returns an exception

--- src/main/kotlin/dev/vjcbs/bunq2ynab/Main.kt
+++ src/main/kotlin/dev/vjcbs/bunq2ynab/Main.kt
@@ -4,10 +4,16 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
+    val log = logger()
+
     val syncService = SyncService()
 
     while (true) {
-        syncService.syncLastOutGoingTransactions()
+        try {
+            syncService.syncLastOutGoingTransactions()
+        } catch (e: Exception) {
+            log.error("Error syncing", e)
+        }
 
         delay(60000)
     }
